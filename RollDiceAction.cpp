243932bb166ab035068ae7fec4a6e31c55b35731
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2019-12-13 14:28:53 +0200

    Update RollDiceAction.cpp
    display messages and Apply actions

--- RollDiceAction.cpp
+++ RollDiceAction.cpp
@@ -23,6 +23,8 @@ void RollDiceAction::Execute()
 
 	// 1- Check if the Game is ended (Use the GetEndGame() function of pGrid), if yes, make the appropriate action
 	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
 
 	if(pGrid->GetEndGame())
 	{ }
@@ -39,6 +41,28 @@ void RollDiceAction::Execute()
 	Player * pPlayer = pGrid->GetCurrentPlayer();
 	// 4- Move the currentPlayer using function Move of class player
 	pPlayer->Move(pGrid, diceNumber);
+
+	Ladder* l = pPlayer->GetCell()->HasLadder();
+	Snake* s = pPlayer->GetCell()->HasSnake();
+	Card* c = pPlayer->GetCell()->HasCard();
+	if (l != NULL)
+	{
+		pOut->PrintMessage("You have reached a ladder. Click to continue");
+		pIn->GetCellClicked();
+		l->Apply(pGrid, pPlayer);
+	}
+	else if(s != NULL)
+	{
+		pOut->PrintMessage("You have reached a Sanke. Click to continue");
+		pIn->GetCellClicked();
+		s->Apply(pGrid, pPlayer);
+	}
+	else if(c != NULL)
+	{
+		pOut->PrintMessage("You have reached a CARD. Click to continue");
+		pIn->GetCellClicked();
+		c->Apply(pGrid, pPlayer);
+	}
 	// 5- Advance the current player number of pGrid
 	pGrid->AdvanceCurrentPlayer();
 	// NOTE: the above guidelines are the main ones but not a complete set (You may need to add more steps).
