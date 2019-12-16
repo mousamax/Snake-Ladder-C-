ca7ceb2711d6902486ddba5af236fefb7682f0c7
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2019-12-16 13:48:10 +0200

    Add files via upload
    error massege

--- InputDiceValue.cpp
+++ InputDiceValue.cpp
@@ -34,7 +34,8 @@ void InputDiceValue::Execute()
 
 		pGrid->GetOutput()->PrintMessage("please enter a dice value between 1-6 : ");
 		diceValue = pGrid->GetInput()->GetInteger(pGrid->GetOutput());
-
+		if (diceValue >= 1 && diceValue <= 6)
+		{
 		// 3- Get the "current" player from pGrid
 		Player* pPlayer = pGrid->GetCurrentPlayer();
 		// 4- Move the currentPlayer using function Move of class player
@@ -42,6 +43,11 @@ void InputDiceValue::Execute()
 		// 5- Advance the current player number of pGrid
 		pGrid->AdvanceCurrentPlayer();
 		// NOTE: the above guidelines are the main ones but not a complete set (You may need to add more steps).
+		}
+		else
+		{
+			pGrid->PrintErrorMessage("Invalid Input");
+		}
 	}
 }
 
